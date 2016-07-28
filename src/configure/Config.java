b50edb3b9f5e13b3752f2481d53ea4018a08c62f
oleh001 <olehkostiuk001@ukr.net>
2016-07-28 21:05:07 +0200

    second

--- src/configure/Config.java
+++ src/configure/Config.java
@@ -6,13 +6,16 @@ import javax.swing.*;
  * Created by user on 23.07.2016.
  */
 public class Config {
-    private final static Config config =new Config();
-    private Config() {}
-    private String title="Calkulator";
-    private int width=400;
-    private int height=400;
+    private final static Config config = new Config();
 
-    private JFrame jFrame=new JFrame();
+    private Config() {
+    }
+
+    private String title = "Calkulator";
+    private int width = 400;
+    private int height = 400;
+
+    private JFrame jFrame = new JFrame();
 
     public JFrame getjFrame() {
         return jFrame;
--- src/view/Window.java
+++ src/view/Window.java
@@ -8,16 +8,16 @@ import java.awt.*;
 /**
  * Created by user on 23.07.2016.
  */
-public class Window{
+public class Window {
 
-    private Config configure= Config.getConfig();
-    private JFrame jFrame=configure.getjFrame();
+    private Config configure = Config.getConfig();
+    private JFrame jFrame = configure.getjFrame();
     //jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     //jFrame..setTitle("Foto");
 
-    public void setjFrame(){
-        Config conf=getConfigure();
-        JFrame frame=getjFrame();
+    public void setjFrame() {
+        Config conf = getConfigure();
+        JFrame frame = getjFrame();
         frame.setVisible(true);
         frame.setTitle(conf.getTitle());
         frame.setSize(conf.getWidth(), conf.getHeight());
@@ -25,10 +25,10 @@ public class Window{
         frame.setResizable(false);//Default size
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit JFrame
         frame.setLayout(new FlowLayout());
-        JButton butAdd=new JButton("+");
-        JButton butMin=new JButton("-");
-        JButton butMul=new JButton("*");
-        JButton butdiv=new JButton("/");
+        JButton butAdd = new JButton("+");
+        JButton butMin = new JButton("-");
+        JButton butMul = new JButton("*");
+        JButton butdiv = new JButton("/");
         butAdd.setBackground(Color.RED);
         butMin.setBackground(Color.red);
 
@@ -36,14 +36,14 @@ public class Window{
         frame.add(butMin);
         frame.add(butMul);
         frame.add(butdiv);
-        JLabel jLab1=new JLabel("Введіть дію");
+        JLabel jLab1 = new JLabel("Введіть дію");
 
-        JTextField jTextField1=new JTextField(10);
-        JLabel jLab2=new JLabel("Результат");
+        JTextField jTextField1 = new JTextField(10);
+        JLabel jLab2 = new JLabel("Результат");
         frame.add(jLab1);
         frame.add(jTextField1);
         frame.add(jLab2);
-        JTextField jTextField2=new JTextField(10);
+        JTextField jTextField2 = new JTextField(10);
         jTextField2.setBackground(Color.cyan);
         frame.add(jTextField2);
 
